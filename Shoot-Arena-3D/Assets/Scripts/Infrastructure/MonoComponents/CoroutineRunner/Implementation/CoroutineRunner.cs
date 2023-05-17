aaf176b3b19f592f460488a14a24d34869f0cc3f
044Developer <044developer@gmail.com>
2023-05-17 21:22:17 +0300

    added coroutine runner

---
+++ Shoot-Arena-3D/Assets/Scripts/Infrastructure/MonoComponents/CoroutineRunner/Implementation/CoroutineRunner.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace ShootArena.Infrastructure.MonoComponents.CoroutineRunner.Implementation
+{
+    public class CoroutineRunner : MonoBehaviour, ICoroutineRunner
+    {
+    }
+}
